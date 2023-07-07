449617aeea5dd6c1478132555756316829f41ef4
Chris Xiao <xiaodabao233@163.com>
2023-07-07 15:14:51 +0800

    项目文件完善
    漏了几个文件

---
+++ pages/news/news.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		这是news页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	pags {
+		background-color: pink;
+	}
+</style>
