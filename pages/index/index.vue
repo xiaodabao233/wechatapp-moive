4f1f6503dd70c5fc0ce542a8c25006401e3a07cd
Chris Xiao <xiaodabao233@163.com>
2023-07-07 14:50:23 +0800

    更新项目
    改为Uni-app项目，删除微信小程序源代码

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		这是项目页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+page{
+	background-color: bisque;
+}
+</style>
---
+++ pages/require/require.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		这是需求页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: cornflowerblue;
+	}
+</style>
